e0619d74b0f16088e92c62fc74e1b10a43edb72d
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2024-06-24 16:01:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import google.generativeai as genai
 import scratchattach as scratch
 import threading
 import os
-import http.server
-import socketserver
-from http import HTTPStatus
-
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        self.wfile.write(b'Hello world')
-
-
-httpd = socketserver.TCPServer(('', 8080), Handler)
-httpd.serve_forever() # all of this so back4app doesnt keep saying it cant do stuff even though its working
 
 # constants
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
