9470f7d2c1f5facc23fd7d98b95abcbcee349131
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2024-08-15 19:23:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import os
 # constants
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 SESSION_ID = os.getenv("SESSION_ID")
+
+if GOOGLE_API_KEY == None
+{
+    print("Google API Key is null!)
+}
+
+if SESSION_ID == None
+{
+    print("Session ID is null!)
+}
+
 CHARS = (
     [""] * 9
     + [" "]
