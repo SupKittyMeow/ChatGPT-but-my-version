1e168d7fe8b0cdbf1e483fb38586e4f789fd788d
SupKittyMeow <77246128+SupKittyMeow@users.noreply.github.com>
2024-10-10 09:53:20 -0700

    quick change

--- main.py
+++ main.py
@@ -54,8 +54,7 @@ def question(argument1, argument2, argument3, argument4):
         print("Question!", flush=True)
         return generate(argument1, argument2, argument3, argument4)
     except Exception as error:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print('Error :( heres the thing:\n' + exc_type + " line " + exc_tb.tb_lineno, flush=True)
-        return 'Error:' + exc_type.__name__
+        print('Error :( heres the thing:\n' + type(error).__name__, flush=True)
+        return 'Error:' + type(error).__name__
 
 client.run()
